6c5861917be0b74953e3338bc5eef23022eea63d
UTC-HOWARD-CHEN\user <howard.chen@e-utc.com.tw>
2022-10-24 16:44:09 +0800

    [coindesk][chore] 優化寫入回傳資訊執行方法

--- src/main/java/testspringboot/services/CoinDeskService.java
+++ src/main/java/testspringboot/services/CoinDeskService.java
@@ -3,7 +3,6 @@ package testspringboot.services;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -36,7 +35,6 @@ public class CoinDeskService {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-
 	/**
 	 * 呼叫外部API
 	 * 
@@ -51,11 +49,11 @@ public class CoinDeskService {
 	 * 取得幣別資訊(中文, 英文, 匯率, 時間)
 	 * 
 	 * @return
-	 * @throws InvocationTargetException 
-	 * @throws IllegalArgumentException 
-	 * @throws IllegalAccessException 
-	 * @throws SecurityException 
-	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws SecurityException
+	 * @throws NoSuchMethodException
 	 * @throws JsonProcessingException
 	 * @throws JsonMappingException
 	 */
@@ -87,16 +85,8 @@ public class CoinDeskService {
 		Date convertUpdated = null;
 		Date convertUpdatedISO = null;
 		Date convertUpdateduk = null;
-		
-		DateFormat fullDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss z");
 
-		try {
-			convertUpdated = fullDateFormat.parse(time.getUpdated());
-			convertUpdatedISO = fullDateFormat.parse(time.getUpdatedISO());
-			convertUpdateduk = fullDateFormat.parse(time.getUpdateduk());
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+		DateFormat fullDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss z");
 
 		Time convertDataTime = new Time();
 		convertDataTime.setUpdated(convertUpdated.toString());
@@ -119,51 +109,60 @@ public class CoinDeskService {
 	private BpiInfo convertBpi(CoinDeskApiResponse coinDeskApiResponse) {
 		// 取得原始幣別資訊
 		Bpi bpi = coinDeskApiResponse.getBpi();
+
+		Map<String, BpiDesc> map = objectMapper.convertValue(bpi, new TypeReference<Map<String, BpiDesc>>() {
+		});
+
 		// 欲回傳的幣別資訊
 		BpiInfo bpiInfo = new BpiInfo();
+		setBpiInfo(map, bpiInfo);
+		// 從DB取得中文幣別資訊
+		SelectCurrencyResponse selectCurrencyResponse = currencyService.selectCurrencyName();
+		setChineseDescription(selectCurrencyResponse.getSelectCurrencyInfoList(), bpiInfo);
+		return bpiInfo;
+	}
 
-		Map<String, BpiDesc> map = objectMapper.convertValue(bpi, new TypeReference<Map<String, BpiDesc>>() {});
+	private BpiInfo setBpiInfo(Map<String, BpiDesc> map, BpiInfo bpiInfo) {
 		// 將原始幣別資訊映射至回傳資訊
-		for (String currency : map.keySet()) {
-			String first = currency.substring(0,1);
-			String last = currency.substring(1).toLowerCase();
+		map.forEach((currency, bpiDesc) -> {
+			String firstWord = currency.substring(0, 1);
+			String lastWord = currency.substring(1).toLowerCase();
+
+			// 回傳幣別內部資料
+			BpiInfoDesc bpiInfoDesc = new BpiInfoDesc();
+			bpiInfoDesc.setCode(bpiDesc.getCode());
+			bpiInfoDesc.setRate(bpiDesc.getRate());
 			try {
-				// 原始幣別內部資料
-				BpiDesc bpiDesc = map.get(currency);
-				// 回傳幣別內部資料
-				BpiInfoDesc bpiInfoDesc = new BpiInfoDesc();
-				
-				bpiInfoDesc.setCode(bpiDesc.getCode());
-				bpiInfoDesc.setRate(bpiDesc.getRate());
 				// 反射, set需傳入資料型態
-				Method setBpiInfoFieldMethod = bpiInfo.getClass().getMethod("set" + first + last, BpiInfoDesc.class);
+				Method setBpiInfoFieldMethod = bpiInfo.getClass().getMethod("set" + firstWord + lastWord,
+						BpiInfoDesc.class);
 				// 將 幣別內部資料 反射至 欲回傳的幣別資訊
 				setBpiInfoFieldMethod.invoke(bpiInfo, bpiInfoDesc);
 			} catch (Exception e) {
-				
+
 			}
-		} // end for
+		});
 
-		// 從DB取得中文幣別資訊
-		SelectCurrencyResponse selectCurrencyResponse = currencyService.selectCurrencyName();
-		List<SelectCurrencyInfo> selectCurrencyInfoList = selectCurrencyResponse.getSelectCurrencyInfoList();
+		return bpiInfo;
+	}
 
+	private BpiInfo setChineseDescription(List<SelectCurrencyInfo> selectCurrencyInfoList, BpiInfo bpiInfo) {
 		// Field[] field
 		for (SelectCurrencyInfo selectCurrencyInfo : selectCurrencyInfoList) {
-			String first = selectCurrencyInfo.getCurrencyId().substring(0,1);
-			String last = selectCurrencyInfo.getCurrencyId().substring(1).toLowerCase();
+			String firstWord = selectCurrencyInfo.getCurrencyId().substring(0, 1);
+			String lastWord = selectCurrencyInfo.getCurrencyId().substring(1).toLowerCase();
 			try {
 				// 反射, get不需傳入資料型態
-				Method getBpiInfoFieldMethod = bpiInfo.getClass().getMethod("get" + first + last);
+				Method getBpiInfoFieldMethod = bpiInfo.getClass().getMethod("get" + firstWord + lastWord);
 				// 將 幣別內部資料 從 欲回傳的幣別資訊 反射出來
 				BpiInfoDesc bpiInfoDesc = (BpiInfoDesc) getBpiInfoFieldMethod.invoke(bpiInfo);
 				// 將反射出的幣別內部資料 設定 中文幣別
 				bpiInfoDesc.setDescription(selectCurrencyInfo.getCurrencyName());
 			} catch (Exception e) {
-				
+
 			}
 		} // end for
-		
+
 		return bpiInfo;
 	}
 }
