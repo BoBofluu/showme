6936298b66af0c113dbd64f974fe87c5353c854a
howard.chen <shakingbabygg@gmail.com>
2022-10-30 20:09:29 +0800

    [system][feat] Postgressql連線設置

--- src/main/java/testspringboot/repositories/CurrencyRepository.java
+++ src/main/java/testspringboot/repositories/CurrencyRepository.java
@@ -2,7 +2,6 @@ package testspringboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import testspringboot.models.entites.Currency;
 
 @Repository
--- src/main/java/testspringboot/services/CurrencyService.java
+++ src/main/java/testspringboot/services/CurrencyService.java
@@ -1,13 +1,9 @@
 package testspringboot.services;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import testspringboot.models.api.currency.InsertCurrencyRequst;
 import testspringboot.models.api.currency.InsertCurrencyResponse;
 import testspringboot.models.api.currency.SelectCurrencyInfo;
@@ -15,39 +11,42 @@ import testspringboot.models.api.currency.SelectCurrencyResponse;
 import testspringboot.models.entites.Currency;
 import testspringboot.repositories.CurrencyRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CurrencyService {
 
-	@Autowired
-	private CurrencyRepository currencyRepository;
+    @Autowired
+    private CurrencyRepository currencyRepository;
 
-	@Transactional(rollbackFor = Exception.class)
-	public InsertCurrencyResponse insertCurrencyName(InsertCurrencyRequst insertCurrencyRequst) {
+    @Transactional(rollbackFor = Exception.class)
+    public InsertCurrencyResponse insertCurrencyName(InsertCurrencyRequst insertCurrencyRequst) {
 
-		Currency currency = new Currency();
-		currency.setCurrencyId(insertCurrencyRequst.getCurrencyId());
-		currency.setCurrencyName(insertCurrencyRequst.getCurrencyName());
+        Currency currency = new Currency();
+        currency.setCurrencyId(insertCurrencyRequst.getCurrencyId());
+        currency.setCurrencyName(insertCurrencyRequst.getCurrencyName());
 
-		currencyRepository.save(currency);
+        currencyRepository.save(currency);
 //		currencyRepository.findById(insertCurrencyRequst.getCurrencyId());
-		return InsertCurrencyResponse.builder().insertResult("新增幣別對應中文說明完成").build();
-	}
-	
-	@Transactional(rollbackFor = Exception.class)
-	public SelectCurrencyResponse selectCurrencyName() {
+        return InsertCurrencyResponse.builder().insertResult("新增幣別對應中文說明完成").build();
+    }
 
-		List<Currency> currencyList = currencyRepository.findAll();
-		
-		List<SelectCurrencyInfo> currencyInfoList = new ArrayList<>();
-		
-		for (Currency currencyInfo : currencyList) {
-			SelectCurrencyInfo selectCurrencyInfo = new SelectCurrencyInfo();
-			selectCurrencyInfo.setCurrencyId(currencyInfo.getCurrencyId());
-			selectCurrencyInfo.setCurrencyName(currencyInfo.getCurrencyName());
-			
-			currencyInfoList.add(selectCurrencyInfo);
-		}
-		
-		return SelectCurrencyResponse.builder().selectCurrencyInfoList(currencyInfoList).build();
-	}
+    @Transactional(rollbackFor = Exception.class)
+    public SelectCurrencyResponse selectCurrencyName() {
+
+        List<Currency> currencyList = currencyRepository.findAll();
+
+        List<SelectCurrencyInfo> currencyInfoList = new ArrayList<>();
+
+        for (Currency currencyInfo : currencyList) {
+            SelectCurrencyInfo selectCurrencyInfo = new SelectCurrencyInfo();
+            selectCurrencyInfo.setCurrencyId(currencyInfo.getCurrencyId());
+            selectCurrencyInfo.setCurrencyName(currencyInfo.getCurrencyName());
+
+            currencyInfoList.add(selectCurrencyInfo);
+        }
+
+        return SelectCurrencyResponse.builder().selectCurrencyInfoList(currencyInfoList).build();
+    }
 }
