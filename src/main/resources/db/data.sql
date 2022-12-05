7e4bc21dae56af8a6538360e94371df695d04f45
SHIQI <shiqi@mitake.com.tw>
2022-12-05 17:54:06 +0800

    [新增測試資料][new] data.sql

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,3 @@
+insert into tb_type_income (type_name, type_seq, type_color) values (1, 1, 1);
+insert into tb_type_income (type_name, type_seq, type_color) values (2, 2, 2);
+insert into tb_type_income (type_name, type_seq, type_color) values (3, 3, 3);
