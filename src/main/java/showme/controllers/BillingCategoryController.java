1c8f8d206bfbd14bcb8cd7011318685ce72f111b
SHIQI <qweqwe0528@gmail.com>
2023-01-14 18:39:56 +0800

    [system][修改] 修改DB，ENTITY

--- src/main/java/showme/controllers/BillingCategoryController.java
+++ src/main/java/showme/controllers/BillingCategoryController.java
@@ -3,12 +3,12 @@ package showme.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import showme.models.api.Request;
-import showme.models.api.Responses;
-import showme.models.api.billingCategory.GetInitType;
 import showme.services.BillingCategoryService;
-import showme.utils.ResponseUtil;
 
 @RestController
 @RequestMapping("/billingCategory")
@@ -17,11 +17,11 @@ public class BillingCategoryController {
     @Autowired
     private BillingCategoryService billingCategoryService;
 
-    @GetMapping("/initType")
-    @Operation(summary = "初始化頁面下拉選單")
-    public Responses<GetInitType> initType() {
-        return ResponseUtil.success(GetInitType.builder().tbList(billingCategoryService.getInitType()).build());
-    }
+//    @GetMapping("/initType")
+//    @Operation(summary = "初始化頁面下拉選單")
+//    public Responses<GetInitType> initType() {
+//        return ResponseUtil.success(GetInitType.builder().tbList(billingCategoryService.getInitType()).build());
+//    }
 
     @PostMapping("/typeSave")
     @Operation(summary = "類型保存類型保存")
